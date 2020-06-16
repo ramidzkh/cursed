9dbed30a8671a1969077d5739682c941789dd018
jearmstrong21 <jearmstrong21@gmail.com>
2020-06-16 11:03:56 -0700

    more worldgen :sunglasses:

--- src/main/java/cursed/biome/Biomes.java
+++ src/main/java/cursed/biome/Biomes.java
@@ -1,7 +1,13 @@
 package cursed.biome;
 
 import cursed.Cursed;
+import cursed.item.Items;
 import net.fabricmc.fabric.api.biomes.v1.NetherBiomes;
+import net.fabricmc.fabric.api.event.server.ServerTickCallback;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.biome.Biome;
 
@@ -15,5 +21,17 @@ public interface Biomes {
 
     static void initialize() {
         NetherBiomes.addNetherBiome(WITHERED_WASTELAND);
+        ServerTickCallback.EVENT.register(minecraftServer -> {
+            if (minecraftServer.getTicks() % 20 == 0) {
+                minecraftServer.getPlayerManager().getPlayerList().forEach(serverPlayerEntity -> {
+                    if(serverPlayerEntity.getStackInHand(Hand.MAIN_HAND).getItem() == Items.WITHERED_FRAGMENT) return;
+                    if(serverPlayerEntity.getStackInHand(Hand.OFF_HAND).getItem() == Items.WITHERED_FRAGMENT) return;
+                    if (serverPlayerEntity.world.getBiome(new BlockPos(serverPlayerEntity.getPos())) == WITHERED_WASTELAND) {
+                        serverPlayerEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS, 300, 1, false, true, true));
+                        serverPlayerEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 300, 1, false, true, true));
+                    }
+                });
+            }
+        });
     }
 }
--- src/main/java/cursed/biome/WitheredWasteland.java
+++ src/main/java/cursed/biome/WitheredWasteland.java
@@ -1,7 +1,11 @@
 package cursed.biome;
 
 import com.google.common.collect.ImmutableList;
+import cursed.entity.Entities;
+import net.minecraft.block.Blocks;
 import net.minecraft.client.sound.MusicType;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnGroup;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.sound.BiomeAdditionsSound;
 import net.minecraft.sound.BiomeMoodSound;
@@ -9,6 +13,14 @@ import net.minecraft.sound.SoundEvents;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.BiomeEffects;
 import net.minecraft.world.biome.BiomeParticleConfig;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.ProbabilityConfig;
+import net.minecraft.world.gen.carver.Carver;
+import net.minecraft.world.gen.decorator.*;
+import net.minecraft.world.gen.feature.DefaultBiomeFeatures;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.FeatureConfig;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
 import net.minecraft.world.gen.surfacebuilder.SurfaceBuilder;
 
 public class WitheredWasteland extends Biome {
@@ -25,8 +37,8 @@ public class WitheredWasteland extends Biome {
                 .effects(new BiomeEffects.Builder()
                         .waterColor(0x3F76E4)
                         .waterFogColor(0x50533)
-                        .fogColor(0x355040)
-                        .particleConfig(new BiomeParticleConfig(ParticleTypes.ANGRY_VILLAGER, 0.00625F))
+                        .fogColor(0x00FF00) // test
+                        .particleConfig(new BiomeParticleConfig(ParticleTypes.FLAME, 0.00625F))
                         .loopSound(SoundEvents.AMBIENT_BASALT_DELTAS_MOOD)
                         .moodSound(new BiomeMoodSound(SoundEvents.AMBIENT_SOUL_SAND_VALLEY_MOOD, 6000, 8, 2.0D))
                         .additionsSound(new BiomeAdditionsSound(SoundEvents.AMBIENT_SOUL_SAND_VALLEY_ADDITIONS, 1 / 90D))
@@ -34,5 +46,24 @@ public class WitheredWasteland extends Biome {
                         .build())
                 .parent(null)
                 .noises(ImmutableList.of(new Biome.MixedNoisePoint(0.5F, -0.5F, 0.0F, 0.0F, 0.0F))));
+        addSpawn(SpawnGroup.MONSTER, new SpawnEntry(Entities.WONDERING_WITHER, 1, 1, 1));
+        addSpawn(SpawnGroup.MONSTER, new SpawnEntry(EntityType.WITHER_SKELETON, 5, 1, 10));
+        this.addStructureFeature(DefaultBiomeFeatures.FORTRESS);
+        this.addStructureFeature(DefaultBiomeFeatures.NETHER_FOSSIL);
+        this.addStructureFeature(DefaultBiomeFeatures.NETHER_RUINED_PORTAL);
+        this.addStructureFeature(DefaultBiomeFeatures.BASTION_REMNANT);
+        this.addCarver(GenerationStep.Carver.AIR, configureCarver(Carver.NETHER_CAVE, new ProbabilityConfig(0.2F)));
+        this.addFeature(GenerationStep.Feature.VEGETAL_DECORATION, Feature.SPRING_FEATURE.configure(DefaultBiomeFeatures.LAVA_SPRING_CONFIG).createDecoratedFeature(Decorator.COUNT_VERY_BIASED_RANGE.configure(new RangeDecoratorConfig(20, 8, 16, 256))));
+        this.addFeature(GenerationStep.Feature.LOCAL_MODIFICATIONS, Feature.BASALT_PILLAR.configure(FeatureConfig.DEFAULT).createDecoratedFeature(Decorator.COUNT_RANGE.configure(new RangeDecoratorConfig(10, 0, 0, 128))));
+        this.addFeature(GenerationStep.Feature.UNDERGROUND_DECORATION, Feature.SPRING_FEATURE.configure(DefaultBiomeFeatures.NETHER_SPRING_CONFIG).createDecoratedFeature(Decorator.COUNT_RANGE.configure(new RangeDecoratorConfig(8, 4, 8, 128))));
+        this.addFeature(GenerationStep.Feature.UNDERGROUND_DECORATION, Feature.GLOWSTONE_BLOB.configure(FeatureConfig.DEFAULT).createDecoratedFeature(Decorator.LIGHT_GEM_CHANCE.configure(new CountDecoratorConfig(10))));
+        this.addFeature(GenerationStep.Feature.UNDERGROUND_DECORATION, Feature.GLOWSTONE_BLOB.configure(FeatureConfig.DEFAULT).createDecoratedFeature(Decorator.COUNT_RANGE.configure(new RangeDecoratorConfig(10, 0, 0, 128))));
+        this.addFeature(GenerationStep.Feature.UNDERGROUND_DECORATION, Feature.RANDOM_PATCH.configure(DefaultBiomeFeatures.SOUL_SAND_CRIMSON_ROOTS_CONFIG).createDecoratedFeature(Decorator.CHANCE_RANGE.configure(new ChanceRangeDecoratorConfig(1.0F, 0, 0, 128))));
+        this.addFeature(GenerationStep.Feature.UNDERGROUND_DECORATION, Feature.RANDOM_PATCH.configure(DefaultBiomeFeatures.NETHER_FIRE_CONFIG).createDecoratedFeature(Decorator.FIRE.configure(new CountDecoratorConfig(10))));
+        this.addFeature(GenerationStep.Feature.UNDERGROUND_DECORATION, Feature.RANDOM_PATCH.configure(DefaultBiomeFeatures.SOUL_FIRE_CONFIG).createDecoratedFeature(Decorator.FIRE.configure(new CountDecoratorConfig(10))));
+        this.addFeature(GenerationStep.Feature.UNDERGROUND_DECORATION, Feature.ORE.configure(new OreFeatureConfig(OreFeatureConfig.Target.NETHERRACK, Blocks.MAGMA_BLOCK.getDefaultState(), 33)).createDecoratedFeature(Decorator.MAGMA.configure(new CountDecoratorConfig(4))));
+        this.addFeature(GenerationStep.Feature.UNDERGROUND_DECORATION, Feature.SPRING_FEATURE.configure(DefaultBiomeFeatures.ENCLOSED_NETHER_SPRING_CONFIG).createDecoratedFeature(Decorator.COUNT_RANGE.configure(new RangeDecoratorConfig(16, 10, 20, 128))));
+        this.addFeature(GenerationStep.Feature.UNDERGROUND_DECORATION, Feature.ORE.configure(new OreFeatureConfig(OreFeatureConfig.Target.NETHERRACK, Blocks.SOUL_SAND.getDefaultState(), 12)).createDecoratedFeature(Decorator.COUNT_RANGE.configure(new RangeDecoratorConfig(12, 0, 0, 32))));
+        DefaultBiomeFeatures.addNetherMineables(this);
     }
 }
